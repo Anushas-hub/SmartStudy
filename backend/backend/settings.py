876d3697796680fa2f7df20b8e6abbdbfa502ee4
Anusha Yadav <yanusha992@gmail.com>
2026-01-21 17:35:34 +0530

    FIX:added django-cors-headers to resolve frontend API connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'materials',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
